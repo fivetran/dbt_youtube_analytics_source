973afc332a0af37945fb277b368b130fa922a0b8
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2023-12-04 11:00:43 -0600

    databricks catalog add

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -20,7 +20,7 @@ dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 ## UPDATE FOR VARS HERE, IF NO VARS, PLEASE REMOVE
-dbt run --vars '{youtube__using_video_metadata: False, youtube__using_channel_demographics: False}' --target "$db" --full-refresh
+dbt run --vars '{youtube__using_channel_demographics: False}' --target "$db" --full-refresh
 dbt test --target "$db"
 ### END VARS CHUNK, REMOVE IF NOT USING
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
